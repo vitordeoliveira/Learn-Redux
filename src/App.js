01533c12eb710c9237ef881d88705da06a104c3f
Vitor de Oliveira <vitordeoliveira50@gmail.com>
2019-07-11 09:50:20 -0300

    Initial Explanation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-        
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,37 +1,49 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-import {createStore} from 'redux';
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
 
 //STORE -> GLOBALIZED STATE
 
 //ACTION INCREMENT
 const increment = () => {
-    return{
-        type: 'INCREMENT'
-    }
-}
+  return {
+    type: "INCREMENT"
+  };
+};
 
 const decrement = () => {
-    return{
-        type: 'DECREMENT'
-    }
-}
+  return {
+    type: "DECREMENT"
+  };
+};
 
 //REDUCER
-const counter = (state=0, action) => {
-    switch(action.type){
-        
-    }
-}
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+};
+
+let store = createStore(counter);
+
+//Display it in the console
+store.subscribe(() => console.log(store.getState()));
 
 //DISPATCH
 
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
+store.dispatch(decrement());
+store.dispatch(increment());
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
